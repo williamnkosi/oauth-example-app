9412d18e1bbc9c79bcd972fe0a90b70804f228f2
William Nkosi <william.dev.acc@gamil.com>
2022-03-01 23:18:37 -0600

    Setup the _authSign() function

--- lib/app_auth_plugin_page.dart
+++ lib/app_auth_plugin_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
 
 class AppAuthPage extends StatefulWidget {
   AppAuthPage({Key? key}) : super(key: key);
@@ -8,15 +9,47 @@ class AppAuthPage extends StatefulWidget {
 }
 
 class _AppAuthPageState extends State<AppAuthPage> {
+  final FlutterAppAuth _appAuth = FlutterAppAuth();
   final String _clientId = "";
   final String _redirectUrl = "";
   final String _issuer = "";
+
+  final AuthorizationServiceConfiguration _serviceConfiguration =
+      const AuthorizationServiceConfiguration(
+    authorizationEndpoint: 'https://demo.identityserver.io/connect/authorize',
+    tokenEndpoint: 'https://demo.identityserver.io/connect/token',
+    endSessionEndpoint: 'https://demo.identityserver.io/connect/endsession',
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("App Auth Plugin")),
-      body: Column(children: []),
+      body: Center(
+        child: Column(children: [
+          MaterialButton(
+            child: Text("Login"),
+            color: Colors.blue,
+            onPressed: _authSignIn,
+          )
+        ]),
+      ),
     );
+  }
+
+  Future<void> _authSignIn() async {
+    try {
+      final authorizationTokenRequest = AuthorizationTokenRequest(
+          _clientId, _redirectUrl,
+          serviceConfiguration: _serviceConfiguration);
+      final AuthorizationTokenResponse? result =
+          await _appAuth.authorizeAndExchangeCode(authorizationTokenRequest);
+
+      if (result != null) {
+      } else {}
+    } catch (e) {
+      print("-------------->");
+      print(e);
+    }
   }
 }
